5be1d985b4699d9dc99d63cbdc4add0629b98768
Thiago jose <thiago.ads.silva@gmail.com>
2022-07-16 06:21:20 -0300

    Inicio da api

---
+++ ApiCatalogo/ApiCatalogo/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ApiCatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Context;
+/*Essa clase é responsável por fazer todo o mapeamento para o banco de dados esse mapeamente
+ é a criação das tabelas que estão sendo representadas pelos atributos dessa classe*/
+
+/*Uma instância dessa classe representa sessão com o banco de dados*/
+public class AppDbContext : DbContext
+{
+    /*Construtor responsável por fazer a configuração do contexto conforme a classe base*/
+    public AppDbContext (DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+    /*O Dbset representa um conjuto de entidades no contexto  que podem ser  consultadas  no 
+     banco de dados (Criar, Alterar, Excluir, Consultar)*/
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+}
+
